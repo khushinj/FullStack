af2b88b2736f2efd711545581a2d64f913c6054e
Khushi Joshi <132840479+khushinj@users.noreply.github.com>
2025-02-05 23:20:25 +0530

    Update app.js

--- DeliciousBackend/app.js
+++ DeliciousBackend/app.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname));
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: `${process.env.FRONTEND_URL}`,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
 }));
